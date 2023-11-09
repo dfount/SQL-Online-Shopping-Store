658039248ab0bfa378971b96ed4fa2e1180e226c
Damian <130514665+dfount@users.noreply.github.com>
2023-11-08 18:56:29 -0600

    Template file for database.h
    Self named, self explained

---
+++ Template file for database.h
@@ -0,0 +1,26 @@
+
+### 1. `database.h` (Header File)
+
+// database.h
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <string>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+
+class Database {
+private:
+    sql::mysql::MySQL_Driver *driver;
+    sql::Connection *con;
+public:
+    Database();
+    ~Database();
+    bool connect();
+    bool disconnect();
+    bool searchProduct(const std::string& searchTerm);
+    bool addToCart(int productId, int quantity);
+    // Add more functions for database operations as needed
+};
+
+#endif // DATABASE_H
