7d84def8f03e52d63b6f8d6f9e10c17425d8297b
alannvv <150203535+alannvv@users.noreply.github.com>
2023-11-09 16:53:34 -0600

    Update MyForm.h

--- MyForm.h
+++ MyForm.h
@@ -10,12 +10,12 @@ namespace namess {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Summary for MyForm
+	/// Summary for ShoppingCart
 	/// </summary>
-	public ref class MyForm : public System::Windows::Forms::Form
+	public ref class ShoppingCart : public System::Windows::Forms::Form
 	{
 	public:
-		MyForm(void)
+		ShoppingCart(void)
 		{
 			InitializeComponent();
 			//
@@ -27,7 +27,7 @@ namespace namess {
 		/// <summary>
 		/// Clean up any resources being used.
 		/// </summary>
-		~MyForm()
+		~ShoppingCart()
 		{
 			if (components)
 			{
@@ -35,12 +35,6 @@ namespace namess {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
 	protected:
 
 	private:
@@ -57,123 +51,34 @@ namespace namess {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(258, 9);
+			this->label1->Location = System::Drawing::Point(337, 54);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(138, 36);
+			this->label1->Size = System::Drawing::Size(185, 32);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Welcome";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			this->label1->Text = L"shopping cart";
+			this->label1->Click += gcnew System::EventHandler(this, &ShoppingCart::label1_Click);
 			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(153, 266);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(131, 51);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"OK";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
-			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(375, 266);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(161, 49);
-			this->button2->TabIndex = 2;
-			this->button2->Text = L"Cancel";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(147, 103);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(76, 32);
-			this->label2->TabIndex = 3;
-			this->label2->Text = L"Item:";
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(94, 193);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(129, 32);
-			this->label3->TabIndex = 4;
-			this->label3->Text = L"Quantity:";
-			// 
-			// textBox1
-			// 
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->textBox1->Location = System::Drawing::Point(229, 106);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(237, 30);
-			this->textBox1->TabIndex = 5;
-			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
-			// 
-			// textBox2
-			// 
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->textBox2->Location = System::Drawing::Point(229, 193);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(237, 30);
-			this->textBox2->TabIndex = 6;
-			// 
-			// MyForm
+			// ShoppingCart
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(621, 360);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->button1);
+			this->ClientSize = System::Drawing::Size(752, 369);
 			this->Controls->Add(this->label1);
-			this->Name = L"MyForm";
-			this->Text = L"MyForm";
-			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->Name = L"ShoppingCart";
+			this->Text = L"ShoppingCart";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-
-		public: bool switchToForm = false;
-			  public: Item^ item = nullptr;
-					private: System::Void btnTv_Click(System::Object^ sender, System::EventArgs^ e) {
-
-					}
-	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-		this->Close();
-	}
-	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
-	}
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-}
-};
+	};
 }
